b389ec63514829f98aada4da07c82db412a86e12
Joseph Parker <jparker010190@gmail.com>
2018-05-05 16:42:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ class TicTacToe
     user_input.to_i - 1
   end
   def move(board, index, current_player)
-    board[index] = current_player
+    @board[index] = current_player
   end
   def position_taken?(board, location)
     board[location] != " " && board[location] != ""
