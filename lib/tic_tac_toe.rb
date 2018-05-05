398ca7ff64b4652e26bd74fa0e7ed2e59f6bb727
Joseph Parker <jparker010190@gmail.com>
2018-05-05 16:13:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,11 +15,7 @@ class TicTacToe
   ]
 
   def display_board(board)
-    puts" #{board[0]} | #{board[1]} | #{board[2]} "
-    puts"-----------"
-    puts" #{board[3]} | #{board[4]} | #{board[5]} "
-    puts"-----------"
-    puts" #{board[6]} | #{board[7]} | #{board[8]} "
+    board = [" "," "," "," "," "," "," "," "," ",] 
   end
 
   def input_to_index(user_input)
