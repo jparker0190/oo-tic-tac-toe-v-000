feb8d9df525f15d2ddf38762197b42aeafc1f299
Joseph Parker <jparker010190@gmail.com>
2018-05-05 16:44:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ class TicTacToe
     @board[index] = current_player
   end
   def position_taken?(location)
-    board[location] != " " && board[location] != ""
+    @board[location] != " " && @board[location] != ""
   end
   def valid_move?(board, index)
     index.between?(0,8) && !position_taken?(board, index)
